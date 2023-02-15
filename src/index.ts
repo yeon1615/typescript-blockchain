235be9aa2eab0f8e12880db5be2a80107bbb7ca3
Jiyeon Ko <goprawgo@gmail.com>
2023-02-15 17:11:23 +0900

    ✨Feat : 블록체인 코드 완성

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,50 @@
+import crypto from 'crypto';
 
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calcHash(prevHash, height, data);
+  }
+  static calcHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash('sha512').update(toHash).digest('base64');
+  }
+}
+
+class BlockChain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPrevHash() {
+    if (this.blocks.length === 0) return '';
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+  public getBlocks() {
+    return [...this.blocks];
+  }
+}
+
+const blockchain = new BlockChain();
+blockchain.addBlock('첫 번째');
+blockchain.addBlock('두 번째');
+blockchain.addBlock('세 번째');
